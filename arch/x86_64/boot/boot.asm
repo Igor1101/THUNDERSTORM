644046685f330c0ae3188a945d17ed38b57b4c50
Igor1101 <Igor1101@github.com>
2018-07-11 21:53:28 +0300

    changed init stack

--- arch/x86_64/boot/boot.asm
+++ arch/x86_64/boot/boot.asm
@@ -18,7 +18,7 @@ global boot_info
 GREEN equ 0x2
 RED equ 0x4f
 
-STK_SIZE equ 1024 * 1024; 1MB for kernel stack
+STK_SIZE equ 1024*16 ; 16 KB for kernel initialization stack
 PG_SIZE equ 512*8; in bytes
 PG_SIZE_QW equ PG_SIZE/8
 global _start; EBX <-- pointer to boot information format

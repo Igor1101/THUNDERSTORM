5353bfe54e872d836d4839df3c4c02d9632ccb77
Igor1101 <Igor1101@github.com>
2018-07-21 14:40:28 +0300

    Bug of the year: added long mode stack initialization( forgotten to do it before)

--- arch/x86_64/boot/early_kernel_init.asm
+++ arch/x86_64/boot/early_kernel_init.asm
@@ -1,22 +1,25 @@
 ;Copyright (C) 2018  Igor Muravyov <igor.muravyov.2015@gmail.com>
 bits 64
-global early_kernel_init
 extern kputstr_32
 extern start_kernel
 extern boot_magic
 extern boot_info
+extern stack_top
+
+global early_kernel_init
 GREEN equ 0x2
 ; kernel x64 port early initialization
 section .rodata
-  verify_x64: db " Long mode initialized", 0
+verify_x64: db " Long mode initialized", 0
 section .text
 early_kernel_init:
-  mov rax, 0x1122334455667788
-  mov rax, verify_x64
-  mov cl, GREEN
-  call kputstr_32
-  mov rdi,  [boot_magic] ; EAX <-- boot magic (via multiboot spec)
-  mov rsi,  [boot_info]  ; EBX <-- boot info
+        ;LONG MODE STACK REINITIALIZATION
+        mov rsp, stack_top
+        mov rax, verify_x64
+        mov cl, GREEN
+        call kputstr_32
+        mov rdi,  [boot_magic] ; EAX <-- boot magic (via multiboot spec)
+        mov rsi,  [boot_info]  ; EBX <-- boot info
 .start_kernel:
-  call start_kernel
-  jmp .start_kernel
+        call start_kernel
+        jmp .start_kernel

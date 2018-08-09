800cd1b7d1eb21bf5ec3ead3d3108781c21976cb
Igor1101 <Igor1101@github.com>
2018-08-09 10:19:07 +0300

    Added kernel mmap()

--- arch/x86_64/include/asm/memory_mapping.h
+++ arch/x86_64/include/asm/memory_mapping.h
@@ -6,9 +6,14 @@
 
 #include <stdint.h>
 #define PG_SIZE 4096
-#define PG_SIZE_QW (PG_SIZE / sizeof(uintptr_t))
+#define PG_SIZE_QW (PG_SIZE / sizeof(uintptr_t) )
+#define _2MBYTE (2<<20)
+#define _MEM_AVAILABLE (0b10000011)
+#define _2MBYTE_ALIGN(X) while( ( (X) % _2MBYTE ) != 0) X++
+
 /* kernel static memory end */
 #define KERNEL_MEM_END (PG_SIZE_QW / 2 - 1)
+#define MMAP_END (PG_SIZE_QW - 1)
 extern uintptr_t p4_table[PG_SIZE_QW];
 extern uintptr_t p3_table[PG_SIZE_QW];
 extern uintptr_t p2_table[PG_SIZE_QW];
@@ -21,5 +26,13 @@ extern uintptr_t p1_table[PG_SIZE_QW];
 void* last_addr(void);
 
 uintptr_t *map_video(volatile void *addr);
+
+/**
+ * this is far more different from POSIX mmap
+ * void *kmmap(void *addr, size_t length)
+ * @addr: address, which should be mapped (must be pg aligned)
+ * @length: length of mmapped area
+ */
+LIKELY void *kmmap(void *addr, size_t length);
 
 #endif                          /*MEMMAP */
--- arch/x86_64/lld/memory_mapping.c
+++ arch/x86_64/lld/memory_mapping.c
@@ -8,17 +8,16 @@
 #include <TH/die.h>
 #include <asm/memory_mapping.h>
 
-void init_paging(void);
-uintptr_t *map_video(volatile void *addr)
-{                               /*
+/* legacy, removing this */
+/*uintptr_t *map_video(volatile void *addr)
+{                               
                                    in arch/x86_64/boot/boot.asm: 8 * 2MB
                                    used for mapping videoaddr/framebuffer
                                    to kernel memory
                                    virtual addr = 2MB * (PG_SIZE - 8)/4 = 0x3fe00000;
-                                 */
+                                
 
         uint64_t volatile videoaddr = (uint64_t) addr | 0b10000011;
-        /* reserve 8 pages */
         volatile uint64_t *volatile map_addr =
             (void *)&p2_table + PG_SIZE - 8 * sizeof(uint64_t);
         (*map_addr) = videoaddr;        // 1st 2 MB`s : 1
@@ -39,7 +38,7 @@ uintptr_t *map_video(volatile void *addr)
 
         return (uintptr_t *)
             ((uintptr_t) (2 << 20) * (PG_SIZE / sizeof(uintptr_t) - 8));
-}
+}*/
 
 /**
  * void* last_addr(void)
@@ -47,6 +46,33 @@ uintptr_t *map_video(volatile void *addr)
  */
 void* last_addr(void)
 {
-        return (void*)
-                ( *( (uint64_t*)&p2_table  + PG_SIZE / sizeof(uintptr_t) - 9 * sizeof (uint64_t) ) );
+        return (void*)p2_table[KERNEL_MEM_END];
+}
+
+static uint32_t mmap_current = KERNEL_MEM_END + 1;
+/**
+ * this is far more different from POSIX mmap
+ * void *kmmap(void *addr, size_t length)
+ * @addr: address, which should be mapped
+ * @length: length of mmapped area
+ */
+LIKELY void *kmmap(void *addr, size_t length)
+{
+        if(mmap_current >= MMAP_END) {
+                return NULL;
+        }
+        void* virt_addr = (void*)(uintptr_t)(_2MBYTE * mmap_current);
+        uint32_t mmap_old = mmap_current;
+        _2MBYTE_ALIGN(length);
+        size_t _2mbyte_length = length / _2MBYTE;
+        do {
+                p2_table[mmap_current++] = (uintptr_t)addr|_MEM_AVAILABLE;
+                addr += _2MBYTE;
+                if(mmap_current >= MMAP_END) {
+                        mmap_current = mmap_old;
+                        return NULL;
+                }
+        }
+        while(_2mbyte_length-- > 0);
+        return virt_addr;
 }

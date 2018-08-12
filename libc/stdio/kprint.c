742784c5715efe418dcd363b57eadac55bbb92cf
Igor1101 <Igor1101@github.com>
2018-08-12 11:00:36 +0300

    Added vsnprintf()

--- libc/stdio/kprint.c
+++ libc/stdio/kprint.c
@@ -1,8 +1,9 @@
 /* *
  * copied from x86k system <https://github.com/alnyan/x86k> 
  * Authors: Alnyan <alnyan@protonmail.com>
+ * Modified: Igor1101
  * (PUBLIC DOMAIN)
- * modified for THUNDERSTORM
+ * modified for THUNDERSTORM System
  * */
 #include <stdint.h>
 #include <kstring.h>
@@ -11,58 +12,54 @@
 #include <kstdio.h>
 #include <TH/lld.h>
 #include <compiler_opt.h>
-#define kputc kputchar
 
-LIKELY void dputs(const char *s)
+LIKELY void kputs(const char *s)
 {
-#ifdef KDEBUG
-        kpause();
-#endif
         while (*s) {
-                kputc(*s++);
+                kputchar(*s++);
         }
 }
 
-LIKELY void dputs_pad(const char *s, char c, size_t p)
+LIKELY void kputs_pad(const char *s, char c, size_t p)
 {
         size_t d = strlen(s);
         if (d < p) {
                 for (size_t i = 0; i < p - d; ++i) {
-                        kputc(c);
+                        kputchar(c);
                 }
         }
-        dputs(s);
+        kputs(s);
 }
 
-LIKELY void ddump(const void *block, size_t count)
+LIKELY void kdump(const void *block, size_t count)
 {
         const char *v = (const char *)block;
         unsigned char c;
         static char cbuf[16];
         for (size_t off = 0; off < count; off += 16) {
                 itoap(cbuf, 8, 16, off);
-                dputs(cbuf);
-                dputs(": ");
+                kputs(cbuf);
+                kputs(": ");
                 for (size_t i = 0; i < 16 && off + i < count; ++i) {
                         c = v[i + off];
                         itoap(cbuf, 2, 16, c);
-                        dputs(cbuf);
-                        kputc(' ');
+                        kputs(cbuf);
+                        kputchar(' ');
                 }
 
-                dputs("   ");
+                kputs("   ");
 
                 for (size_t i = 0; i < 16 && off + i < count; ++i) {
                         c = v[i + off];
                         if (!c) {
-                                kputc('.');
+                                kputchar('.');
                         } else if (c > 32 && c < 0x7F) {
-                                kputc(c);
+                                kputchar(c);
                         } else {
-                                kputc('?');
+                                kputchar('?');
                         }
                 }
-                kputc('\n');
+                kputchar('\n');
         }
 }
 
@@ -74,11 +71,155 @@ void kprintf(const char *fmt, ...)
         va_end(args);
 }
 
-LIKELY void kvprintf(const char *fmt, va_list args)
+LIKELY int vsnprintf (char * s, size_t n, 
+                const char * fmt, va_list args )
 {
+        /* it does not seem to get overflowed */
+#define CBUF_SIZE 256
         char c;
         uintptr_t vp;
-        char cbuf[256];
+        static char cbuf[CBUF_SIZE];
+        char padc;
+        int padn;
+        char*buf_p = s;
+
+        auto int kputchar_buf(char c);
+        auto int kputs_pad_buf(const char *s, char c, size_t p);
+        auto int kputchar_buf(char c);
+        auto int kputs_buf(const char *s);
+
+        auto int kputchar_buf(char c)
+        {
+                *(buf_p++) = c;
+                if( (uintptr_t)(buf_p - s) >= n )
+                        return EXIT_FAILURE;
+                return EXIT_SUCCESS;
+        }
+
+        auto int kputs_pad_buf(const char *s, char c, size_t p)
+        {
+                size_t d = strlen(s);
+                if (d < p) {
+                        for (size_t i = 0; i < p - d; ++i) {
+                                if(kputchar_buf(c) == EXIT_FAILURE) {
+                                        return EXIT_FAILURE;
+                                }
+                        }
+                }
+                if(kputs_buf(s) == EXIT_FAILURE) {
+                        return EXIT_FAILURE;
+                }
+                return EXIT_SUCCESS;
+        }
+
+        auto int kputs_buf(const char *s)
+        {
+                while (*s) {
+                        if(kputchar_buf(*s++) == EXIT_FAILURE) {
+                                return EXIT_FAILURE;
+                        }
+                }
+                return EXIT_SUCCESS;
+        }
+
+        while ((c = *fmt)) {
+                switch (c) {
+                case '%':
+                        ++fmt;
+                        c = *fmt;
+                        padc = 0;
+                        if (c == '0' || c == ' ') {
+                                padc = c;
+                                ++fmt;
+                        }
+                        padn = 0;
+                        c = *fmt;
+                        while (c >= '0' && c <= '9') {
+                                padn *= 10;
+                                padn += c - '0';
+                                c = *++fmt;
+                        }
+
+                        if (padn && padc) {
+                                switch (c) {
+                                case 'x':
+                                        vp = va_arg(args, uintptr_t);
+                                        itoa(cbuf, 16, vp);
+                                        if(kputs_pad_buf(cbuf, padc, padn) 
+                                                        == EXIT_FAILURE) {
+                                                return (buf_p - s);
+                                        }
+                                        break;
+                                default:
+                                        if(kputchar_buf('%') == EXIT_FAILURE ) {
+                                                return (buf_p - s);
+                                        }
+                                        if(kputchar_buf(c) == EXIT_FAILURE ) {
+                                                return (buf_p - s);
+                                        }
+                                        break;
+                                }
+                        } else {
+                                switch (c) {
+                                case 'a':
+                                        vp = va_arg(args, uintptr_t);
+                                        itoa(cbuf, 16, vp);
+                                        if(kputs_buf("0x") == EXIT_FAILURE)
+                                                return (buf_p - s);
+                                        if(kputs_pad_buf(cbuf, '0', 8) == EXIT_FAILURE)
+                                                return (buf_p - s);
+                                        break;
+                                case 'd':
+                                        vp = va_arg(args, uintptr_t);
+                                        itoa(cbuf, 10, vp);
+                                        if(kputs_buf(cbuf) == EXIT_FAILURE) 
+                                                return (buf_p - s);
+                                        break;
+                                case 'x':
+                                        vp = va_arg(args, uintptr_t);
+                                        itoa(cbuf, 16, vp);
+                                        if(kputs_buf(cbuf) == EXIT_FAILURE)
+                                                return (buf_p - s);
+                                        break;
+                                case 'u':
+                                        vp = va_arg(args, uintptr_t);
+                                        utoa(cbuf, vp);
+                                        if(kputs_buf(cbuf) == EXIT_FAILURE)
+                                                return (buf_p - s);
+                                        break;
+                                case 's':
+                                        vp = (uintptr_t) va_arg(args,
+                                                                const char *);
+                                        if(kputs_buf(vp ? (const char *)vp : "(NULL)")
+                                                        == EXIT_FAILURE)
+                                                return (buf_p - s);
+                                        break;
+                                default:
+                                        if(kputchar_buf('%') == EXIT_FAILURE)
+                                                return (buf_p - s);
+                                        if(kputchar_buf(c) == EXIT_FAILURE)
+                                                return (buf_p - s);
+                                        break;
+                                }
+                        }
+                        break;
+                default:
+                        if(kputchar_buf(c) == EXIT_FAILURE) 
+                                return (buf_p - s);
+                        break;
+                }
+                ++fmt;
+        }
+        return (buf_p - s);
+}
+
+LIKELY void kvprintf(const char *fmt, va_list args)
+{
+        /* it does not seem to be get overflowed */
+#define CBUF_SIZE 256
+        char c;
+        uintptr_t vp;
+        char cbuf[CBUF_SIZE];
         char padc;
         int padn;
 
@@ -105,11 +246,11 @@ LIKELY void kvprintf(const char *fmt, va_list args)
                                 case 'x':
                                         vp = va_arg(args, uintptr_t);
                                         itoa(cbuf, 16, vp);
-                                        dputs_pad(cbuf, padc, padn);
+                                        kputs_pad(cbuf, padc, padn);
                                         break;
                                 default:
-                                        kputc('%');
-                                        kputc(c);
+                                        kputchar('%');
+                                        kputchar(c);
                                         break;
                                 }
                         } else {
@@ -117,38 +258,38 @@ LIKELY void kvprintf(const char *fmt, va_list args)
                                 case 'a':
                                         vp = va_arg(args, uintptr_t);
                                         itoa(cbuf, 16, vp);
-                                        dputs("0x");
-                                        dputs_pad(cbuf, '0', 8);
+                                        kputs("0x");
+                                        kputs_pad(cbuf, '0', 8);
                                         break;
                                 case 'd':
                                         vp = va_arg(args, uintptr_t);
                                         itoa(cbuf, 10, vp);
-                                        dputs(cbuf);
+                                        kputs(cbuf);
                                         break;
                                 case 'x':
                                         vp = va_arg(args, uintptr_t);
                                         itoa(cbuf, 16, vp);
-                                        dputs(cbuf);
+                                        kputs(cbuf);
                                         break;
                                 case 'u':
                                         vp = va_arg(args, uintptr_t);
                                         utoa(cbuf, vp);
-                                        dputs(cbuf);
+                                        kputs(cbuf);
                                         break;
                                 case 's':
                                         vp = (uintptr_t) va_arg(args,
                                                                 const char *);
-                                        dputs(vp ? (const char *)vp : "(null)");
+                                        kputs(vp ? (const char *)vp : "(null)");
                                         break;
                                 default:
-                                        kputc('%');
-                                        kputc(c);
+                                        kputchar('%');
+                                        kputchar(c);
                                         break;
                                 }
                         }
                         break;
                 default:
-                        kputc(c);
+                        kputchar(c);
                         break;
                 }
                 ++fmt;
